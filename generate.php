9e76261dcc5f0ed7fd4d10a3d7945fd654b9ec02
Alex Yashkin <alex.yashkin@gmail.com>
2018-09-26 16:15:46 +0300

    English is optional too

--- generate.php
+++ generate.php
@@ -10,7 +10,7 @@ $en_locales = require $locales_path . '/en/locales.php';
 
 $add_locales = [];
 foreach ($add_langs as $add_lang) {
-    if (strlen($add_lang) == 2 && preg_match('/^[a-z]{2}$/', $add_lang)) {
+    if (strlen($add_lang) == 2 && preg_match('/^[a-z]{2}$/', $add_lang) && $add_lang != 'en') {
         $add_locales_path = $locales_path . '/' . $add_lang . '/locales.php';
         $add_locales[$add_lang] = is_file($add_locales_path) ? require $add_locales_path : [];
     }
@@ -27,9 +27,11 @@ foreach ($en_locales as $locale => $name) {
             $out[$locale_dash] = [
                 'name' => $locale_locales[$short_locale],
                 'full_name' => $locale_locales[$locale],
-                'name_en' => $en_locales[$short_locale],
-                'full_name_en' => $name,
             ];
+            if (in_array('en', $add_langs)) {
+                $out[$locale_dash]['name_en'] = $en_locales[$short_locale];
+                $out[$locale_dash]['full_name_en'] = $name;
+            }
             foreach ($add_langs as $add_lang) {
                 if (isset($add_locales[$add_lang][$locale], $add_locales[$add_lang][$short_locale])) {
                     $out[$locale_dash]['name_' . $add_lang] = $add_locales[$add_lang][$short_locale];
